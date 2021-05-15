df3dd2021b6ecdb8e72b8240b32b886dcc5f888c
강민규 <gangmingyu@gangmingyuui-MacBookAir.local>
2021-05-16 08:11:11 +0900

    newslist component 작성

--- newsapi/news-viewer/src/App.js
+++ newsapi/news-viewer/src/App.js
@@ -1,30 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
 import { useState } from 'react';
 import axios from 'axios';
+import NewsList from './components/NewsList';
 
 const App = () => {
   const [data, setData] = useState(null);
-  const onClick = () => {
-    axios
-      .get('https://jsonplaceholder.typicode.com/todos/1')
-      .then((response) => {
-        setData(response.data);
-      });
+  const onClick = async () => {
+    try {
+      const response = await axios.get(
+        'https://newsapi.org/v2/top-headlines?country=kr&apiKey=bfc35f0de1be498185c9c5a4b48e2d50',
+      );
+      setData(response.data);
+    } catch (e) {
+      console.log(e);
+    }
   };
 
   return (
     <div>
-      <div>
-        <button onClick={onClick}>불러오기</button>
-      </div>
-      {data && (
-        <textarea
-          rows={7}
-          value={JSON.stringify(data, null, 2)}
-          readOnly={true}
-        />
-      )}
+      <NewsList />
     </div>
   );
 };
